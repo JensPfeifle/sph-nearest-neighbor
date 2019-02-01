726e3fe272f6e859c6afbf4afa6b3481ab590958
JensPfeifle <jens@pfeifle.tech>
2019-02-01 11:35:48 +0100

    docs update

--- latex/autobuild.sh
+++ latex/autobuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . name - '*.tex' | entr pdflatex \\nonstopmode\\input gesamt.tex
+find -L . -type f -name '*.tex' -or -name '*.pdf' -and ! -name 'gesamt.pdf'  | entr pdflatex \\nonstopmode\\input gesamt.tex
 
